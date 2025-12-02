80067a7b012e7d5f499dd8c6f3b4d6f857400aa6
royer14-cloud <moriland11@hotmail.com>
2025-12-02 15:11:18 -0500

    agregando comentarios

--- load_dir.py
+++ load_dir.py
@@ -5,9 +5,9 @@ import sys
 
 def load_init():
     global BASE_DIR
-    if hasattr(sys, '_MEIPASS'):
+    if hasattr(sys, '_MEIPASS'): # si es onefile
         BASE_DIR = sys._MEIPASS
-    elif getattr(sys, 'frozen',False):
+    elif getattr(sys, 'frozen',False): # si es onedir
         BASE_DIR = os.path.dirname(sys.executable)
     else:
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
